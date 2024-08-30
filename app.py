99a437a7b4a42275128946ee84ef6e98d4a5aad8
daniyalumer <daniyalharal100@gmail.com>
2024-08-30 11:41:25 +0500

    Updates

--- app.py
+++ app.py
@@ -1,64 +1,74 @@
-#create, read, update and delete --- create and update can be under the same. if id given update else create
-# id, description, date created, date updated, find the hash of the description to put in id.
-#crud operations need to be done 
-
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from datetime import datetime
 import hashlib
 
 app = Flask(__name__)
 
-todo_list=[]
+todo_list = []
+
 
 def generatehash(description):
-    return hashlib.md5(description.encode('utf-8')).hexdigest()
+    return hashlib.md5(description.encode("utf-8")).hexdigest()
 
 
-@app.route('/todo', methods=['POST'])
+@app.route("/todo", methods=["POST"])
 def create_or_update_todo():
-    description=request.form.get('description')
-    id=request.form.get('id')
-    currenttime=str(datetime.now())[:-7]
-    #update item
+    # TODO: should be coming from the request body instead
+    description = request.form.get("description")
+    id = request.form.get("id")
+    currenttime = str(datetime.now())[:-7]
+
+    # TabularJSON.com
+    # Postman application
+
+    # update item
     if id:
         for todo in todo_list:
-            if id == todo['id']:
-                todo['id']=generatehash(description)
-                todo['description']=description
-                todo['dateupdated']=currenttime
-                return redirect(url_for('home'))
-            return jsonify({'error': 'Todo item not found'}), 404
-    #create item
+            if id == todo["id"]:
+                todo["id"] = generatehash(description)
+                todo["description"] = description
+                todo["dateupdated"] = currenttime
+                return redirect(url_for("home"))
+            return jsonify({"error": "Todo item not found"}), 404
+
+    # create item
     else:
-        todo_id=generatehash(description)
+        todo_id = generatehash(description)
         for todo in todo_list:
-            if todo['id']==todo_id:
-                return jsonify({'error': 'Item with the same description already exists'}), 400
-        
+            if todo["id"] == todo_id:
+                return (
+                    jsonify({"error": "Item with the same description already exists"}),
+                    400,
+                )
+
         new_todo_item = {
-            'id': todo_id,
-            'description': description,
-            'date_created': currenttime,
-            'date_updated': currenttime,
-            'date_completed': None,
-            'completed': False
+            "id": todo_id,
+            "description": description,
+            "date_created": currenttime,
+            "date_updated": currenttime,
+            "date_completed": None,
+            "completed": False,
         }
         todo_list.append(new_todo_item)
-        return redirect(url_for('home'))
+        return redirect(url_for("home"))
 
-@app.route('/completed/<string:todo_id>', methods=['POST'])
+
+# this should be done in the /todo POST updation block
+@app.route("/completed/<string:todo_id>", methods=["POST"])
 def completed_todo_by_id(todo_id):
-    current_time=str(datetime.now())[0:7]
+    current_time = str(datetime.now())[0:7]
     for todo in todo_list:
-        if todo['id']==todo_id:
-            todo['completed']=True,
-            todo['date_completed']=current_time
-            return redirect(url_for('home'))
-    return jsonify({'error': 'Todo item not found'}), 404
+        if todo["id"] == todo_id:
+            todo["completed"] = (True,)
+            todo["date_completed"] = current_time
+            return redirect(url_for("home"))
+    return jsonify({"error": "Todo item not found"}), 404
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('index.html', todos=todo_list)
+    return render_template("index.html", todos=todo_list)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
