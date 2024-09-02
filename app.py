f4e856ab3c1cba46766c98eca24cc1bbdc01e91b
daniyalumer <daniyalharal100@gmail.com>
2024-09-02 13:57:52 +0500

    Improved The UI And Resolved Current Time Bug

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from datetime import datetime
 import hashlib
+import sys
 
 app = Flask(__name__)
 
@@ -10,13 +11,27 @@ todo_list = []
 def generatehash(description):
     return hashlib.md5(description.encode("utf-8")).hexdigest()
 
+def get_current_time():
+    return str(datetime.now)[:-7]
 
 @app.route("/todo", methods=["POST"])
 def create_or_update_todo():
     # TODO: should be coming from the request body instead
     description = request.form.get("description")
     id = request.form.get("id")
-    currenttime = str(datetime.now())[:-7]
+    current_time = str(datetime.now())[:-7]
+    completed=request.form.get("completed")
+
+    print(f"Received ID: {id}", file=sys.stderr)  # Debugging: Print the received ID
+
+    # mark as completed
+    if completed:
+        for todo in todo_list:
+            if todo["id"] == id:
+                todo["completed"] = True
+                todo["date_completed"] = current_time
+                return redirect(url_for("home"))
+        return jsonify({"error": "Todo item not found"}), 404
 
     # TabularJSON.com
     # Postman application
@@ -24,12 +39,13 @@ def create_or_update_todo():
     # update item
     if id:
         for todo in todo_list:
+            print(f"UPDATE- Received ID: {todo['id']}", file=sys.stderr) 
             if id == todo["id"]:
                 todo["id"] = generatehash(description)
                 todo["description"] = description
-                todo["dateupdated"] = currenttime
+                todo["dateupdated"] = current_time
                 return redirect(url_for("home"))
-            return jsonify({"error": "Todo item not found"}), 404
+        return jsonify({"error": "Todo item not found"}), 404
 
     # create item
     else:
@@ -44,8 +60,8 @@ def create_or_update_todo():
         new_todo_item = {
             "id": todo_id,
             "description": description,
-            "date_created": currenttime,
-            "date_updated": currenttime,
+            "date_created": current_time,
+            "date_updated": current_time,
             "date_completed": None,
             "completed": False,
         }
@@ -54,7 +70,7 @@ def create_or_update_todo():
 
 
 # this should be done in the /todo POST updation block
-@app.route("/completed/<string:todo_id>", methods=["POST"])
+""" @app.route("/completed/<string:todo_id>", methods=["POST"])
 def completed_todo_by_id(todo_id):
     current_time = str(datetime.now())[0:7]
     for todo in todo_list:
@@ -62,7 +78,7 @@ def completed_todo_by_id(todo_id):
             todo["completed"] = (True,)
             todo["date_completed"] = current_time
             return redirect(url_for("home"))
-    return jsonify({"error": "Todo item not found"}), 404
+    return jsonify({"error": "Todo item not found"}), 404 """
 
 
 @app.route("/")
